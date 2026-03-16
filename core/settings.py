ab3bf49f5d7d2d994df78e57e0b178044fcb8088
BoastfulApex <88885919+BoastfulApex@users.noreply.github.com>
2026-03-16 15:04:48 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -163,3 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://sportedu.boastful.uz',
+]
