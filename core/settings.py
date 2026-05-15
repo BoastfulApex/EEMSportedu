c7df4af68af3adc7b41a31d880a7e4ba0e10b548
BoastfulApex <88885919+BoastfulApex@users.noreply.github.com>
2026-05-15 10:36:16 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -165,4 +165,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CSRF_TRUSTED_ORIGINS = [
     'https://sportedu.boastful.uz',
     'https://kpi.sportedu.uz',
+    'https://kpi.boastful.uz',
 ]
