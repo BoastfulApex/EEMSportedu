252343781d4d126ed83df55583073466311c5bc5
BoastfulApex <88885919+BoastfulApex@users.noreply.github.com>
2026-05-18 16:15:05 +0500

    update handlers/users/__init__.py

--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -3,5 +3,6 @@ from . import stats
 from . import reports
 from . import student_attendance
 from . import student_login   # tinglovchi login/parol orqali kirish
+from . import employee_attendance  # xodim filiali davomati
 
 __all__ = ["dp"]
