d8064a21d293112d7b169831565b4753478eaf51
BoastfulApex <88885919+BoastfulApex@users.noreply.github.com>
2026-05-19 16:47:02 +0500

    update apps/home/views.py

--- apps/home/views.py
+++ apps/home/views.py
@@ -1647,14 +1647,7 @@ def _get_para_attendance(att, lesson):
     smena = lesson.smena
     date  = lesson.date
 
-    slots = smena.get_slots()
-    para_starts = [s.start for s in slots if s.start]
-    if not para_starts and smena.para1_start:
-        para_starts = [smena.para1_start]
-        if smena.para2_start:
-            para_starts.append(smena.para2_start)
-        if smena.para3_start:
-            para_starts.append(smena.para3_start)
+    para_starts = _get_smena_para_starts(smena)
 
     result = {'p1': None, 'p2': None, 'p3': None}
     labels = ['p1', 'p2', 'p3']
